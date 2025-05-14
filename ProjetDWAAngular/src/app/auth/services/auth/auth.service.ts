6851a27e570f35dc8735ae25bf2c004efe319984
mathieu-gtz <mathieu.gaintza@gmail.com>
2025-05-14 03:45:06 +0200

    modif apiUrl dasn auth service

--- ProjetDWAAngular/src/app/auth/services/auth/auth.service.ts
+++ ProjetDWAAngular/src/app/auth/services/auth/auth.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment.prod';
+import { Observable } from 'rxjs';
 
-const BASE_URL = "environment.apiUrl";
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
-  signup(signupRequest:any):Observable<any>{
-    return this.http.post(BASE_URL + "api/auth/signup", signupRequest);
+  signup(signupRequest: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/api/auth/signup`, signupRequest);
   }
 
-  login(loginRequest:any):Observable<any>{
-    return this.http.post(BASE_URL + "api/auth/login", loginRequest);
+  login(loginRequest: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/api/auth/login`, loginRequest);
   }
 }
